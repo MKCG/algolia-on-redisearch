c71cfc1cc9d074c6586c614cc002a5f6bb54a86a
Kévin Masseix <masseix.kevin@gmail.com>
2020-04-28 17:36:19 +0200

    [Template] Refine facets and render results as table

--- api/assets/js/app.js
+++ api/assets/js/app.js
@@ -27,18 +27,146 @@ const search = instantsearch({
     searchClient: client,
 });
 
+const renderHits = (renderOptions, isFirstRender) => {
+  const { hits, widgetParams } = renderOptions;
+
+  document.querySelector(widgetParams.container).innerHTML = `
+    <table class="mdl-data-table mdl-js-data-table">
+      <thead>
+        <tr>
+          <th class="mdl-data-table__cell--non-numeric">Titre</th>
+          <th>Année de Début</th>
+          <th>Année de Fin</th>
+          <th>Durée (minutes)</th>
+        </tr>
+      </thead>
+      <tbody>
+        ${hits
+          .map(
+            item =>
+              `<tr>
+                <td>${item.primaryTitle}</td>
+                <td>${item.startYear}</td>
+                <td>` + (item.endYear ? item.endYear : item.startYear) + `</td>
+                <td>${item.runtimeMinutes}</td>
+              </tr>`
+          )
+          .join('')}
+      </tbody>
+    </table>
+  `;
+};
+
+const renderRefinementList = (renderOptions, isFirstRender) => {
+  const { items , refine } = renderOptions;
+
+  let container = document.querySelector(renderOptions.widgetParams.container);
+
+  container.innerHTML = `
+    <ul class="mdl-list facet-terms">
+      ${items
+        .map(
+          (item, idx) => `
+            <li
+              class="mdl-list__item"
+              data-value="${item.value}"
+            >
+              <span class="mdl-list__item-secondary-action">
+                <label
+                  class="mdl-checkbox mdl-js-checkbox mdl-js-ripple-effect"
+                  for="facet-${renderOptions.widgetParams.attribute}-${idx}">
+                  <input
+                    id="facet-${renderOptions.widgetParams.attribute}-${idx}"
+                    type="checkbox"
+                    class="mdl-checkbox__input"`
+                    + (item.isRefined ? ` checked`: ``) + `
+                   />
+                </label>
+              </span>
+              <span class="mdl-list__item-primary-content">
+                ${item.label} (${item.count})
+              </span>
+            </li>`
+        )
+        .join('')}
+    </ul>
+  `;
+
+  [...container.querySelectorAll('li')].forEach(element => {
+    element.addEventListener('click', event => {
+      refine(event.currentTarget.dataset.value);
+    });
+  });
+};
+
+const renderCurrentRefinements = (renderOptions, isFirstRender) => {
+  const { items , refine } = renderOptions;
+
+  const createDataAttribtues = refinement =>
+    Object.keys(refinement)
+      .map(key => `data-${key}="${refinement[key]}"`)
+      .join(' ');
+
+  let container = document.querySelector(renderOptions.widgetParams.container);
+
+  let refinements = items.map((item) => {
+    return item.refinements.map((refinement, idx) => `
+        <span
+          class="mdl-chip mdl-chip--deletable facet-refinement"
+          ${createDataAttribtues(refinement)}
+        >
+          <span class="mdl-chip__text">${refinement.label}</span>
+          <button type="button" class="mdl-chip__action"><i class="material-icons">cancel</i></button>
+        </span>
+      `).join('')
+  });
+
+  container.innerHTML = refinements.join('');
+
+  [...container.querySelectorAll('.facet-refinement')].forEach(element => {
+    element.addEventListener('click', event => {
+      const item = Object.keys(event.currentTarget.dataset).reduce(
+        (acc, key) => ({
+          ...acc,
+          [key]: event.currentTarget.dataset[key],
+        }),
+        {}
+      );
+
+      refine(item);
+    });
+  });
+};
+
+const customHits = instantsearch.connectors.connectHits(renderHits);
+
+const customRefinementList = instantsearch.connectors.connectRefinementList(
+  renderRefinementList
+);
+
+const currentRefinements = instantsearch.connectors.connectCurrentRefinements(
+  renderCurrentRefinements
+);
 
 search.addWidgets([
-  instantsearch.widgets.currentRefinements({
+ instantsearch.widgets.searchBox({
+      container: '#searchbox',
+      placeholder: 'Rechercher...',
+      showSubmit: false,
+      showReset: false,
+      showLoadingIndicator: false
+  }),
+
+  currentRefinements({
     container: '#current-refinements',
   }),
 
-  instantsearch.widgets.refinementList({
+  customRefinementList({
     container: '#facet-titleType',
     attribute: 'titleType',
   }),
 
-  instantsearch.widgets.refinementList({
+  customRefinementList({
     container: '#facet-genres',
     attribute: 'genres',
   }),
@@ -46,28 +174,15 @@ search.addWidgets([
   instantsearch.widgets.hitsPerPage({
     container: '#hits-per-page',
     items: [
-      { label: '8 hits per page', value: 8, default: true },
-      { label: '16 hits per page', value: 16 },
+      { label: '20 hits per page', value: 20, default: true },
+      { label: '50 hits per page', value: 50 },
+      { label: '100 hits per page', value: 100 },
     ],
   }),
 
-  instantsearch.widgets.hits({
+  customHits({
     container: '#hits',
-    templates: {
-      item: `
-          <span class="hit-name">{{ primaryTitle }}</span>
-          <span class="hit-startYear">{{ startYear }}</span>
-          <span class="hit-endYear">{{ endYear }}</span>
-          <span class="hit-runtimeMinutes">{{ runtimeMinutes }}</span>
-      `,
-    },
-  })
+  }),
 ]);
-
-search.addWidget(
-    instantsearch.widgets.searchBox({
-        container: '#searchbox'
-    })
-);
 
 search.start();
